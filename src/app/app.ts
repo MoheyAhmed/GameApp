c213d3ec1d49d3c9a42f4aadd5def4e24f4e5e45
MoheyAhmed <ma7838877@gmail.com>
2025-07-28 23:51:23 +0300

    test without footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Footer, RouterOutlet],
+  imports: [Navbar,  RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
